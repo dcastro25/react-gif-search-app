6c4e071afb9f2728aafab37e903d1cffbd733ea4
Oscar Castro <ibanezoscar157@gmail.com>
2026-02-16 14:54:02 -0500

    fix: eliminado enum y ajustadas interfaces para compatibilidad con build

--- src/gifs/interface/giphy.response.ts
+++ src/gifs/interface/giphy.response.ts
@@ -1,114 +1,117 @@
 export interface GiphyResponse {
-    data:       GiphyGif[];
-    meta:       Meta;
-    pagination: Pagination;
+  data: GiphyGif[];
+  meta: Meta;
+  pagination: Pagination;
 }
 
 export interface GiphyGif {
-    type:                       Type;
-    id:                         string;
-    url:                        string;
-    slug:                       string;
-    bitly_gif_url:              string;
-    bitly_url:                  string;
-    embed_url:                  string;
-    username:                   string;
-    source:                     string;
-    title:                      string;
-    rating:                     Rating;
-    content_url:                string;
-    source_tld:                 string;
-    source_post_url:            string;
-    is_sticker:                 number;
-    import_datetime:            Date;
-    trending_datetime:          string;
-    images:                     Images;
-    analytics_response_payload: string;
-    analytics:                  Analytics;
-    alt_text:                   string;
-    is_low_contrast:            boolean;
+  type: Type;
+  id: string;
+  url: string;
+  slug: string;
+  bitly_gif_url: string;
+  bitly_url: string;
+  embed_url: string;
+  username: string;
+  source: string;
+  title: string;
+  rating: Rating;
+  content_url: string;
+  source_tld: string;
+  source_post_url: string;
+  is_sticker: number;
+
+  import_datetime: string;
+  trending_datetime: string;
+
+  images: Images;
+  analytics_response_payload: string;
+  analytics: Analytics;
+  alt_text: string;
+  is_low_contrast: boolean;
 }
 
+export type Rating = "g" | "pg" | "pg-13" | "r";
+
+export type Type = "gif";
 export interface Analytics {
-    onload:  Onclick;
-    onclick: Onclick;
-    onsent:  Onclick;
+  onload: AnalyticsAction;
+  onclick: AnalyticsAction;
+  onsent: AnalyticsAction;
 }
 
-export interface Onclick {
-    url: string;
+export interface AnalyticsAction {
+  url: string;
 }
 
 export interface Images {
-    original:                 FixedHeight;
-    downsized:                The480_WStill;
-    downsized_large:          The480_WStill;
-    downsized_medium:         The480_WStill;
-    downsized_small:          DownsizedSmall;
-    downsized_still:          The480_WStill;
-    fixed_height:             FixedHeight;
-    fixed_height_downsampled: FixedHeight;
-    fixed_height_small:       FixedHeight;
-    fixed_height_small_still: The480_WStill;
-    fixed_height_still:       The480_WStill;
-    fixed_width:              FixedHeight;
-    fixed_width_downsampled:  FixedHeight;
-    fixed_width_small:        FixedHeight;
-    fixed_width_small_still:  The480_WStill;
-    fixed_width_still:        The480_WStill;
-    looping:                  DownsizedSmall;
-    original_still:           The480_WStill;
-    original_mp4:             DownsizedSmall;
-    preview:                  DownsizedSmall;
-    preview_gif:              The480_WStill;
-    preview_webp:             The480_WStill;
-    "480w_still":             The480_WStill;
+  original: ImageFull;
+  downsized: ImageVariant;
+  downsized_large: ImageVariant;
+  downsized_medium: ImageVariant;
+  downsized_small: VideoVariant;
+  downsized_still: ImageVariant;
+
+  fixed_height: ImageFull;
+  fixed_height_downsampled: ImageFull;
+  fixed_height_small: ImageFull;
+  fixed_height_small_still: ImageVariant;
+  fixed_height_still: ImageVariant;
+
+  fixed_width: ImageFull;
+  fixed_width_downsampled: ImageFull;
+  fixed_width_small: ImageFull;
+  fixed_width_small_still: ImageVariant;
+  fixed_width_still: ImageVariant;
+
+  looping: VideoVariant;
+  original_still: ImageVariant;
+  original_mp4: VideoVariant;
+
+  preview: VideoVariant;
+  preview_gif: ImageVariant;
+  preview_webp: ImageVariant;
+
+  "480w_still": ImageVariant;
 }
 
-export interface The480_WStill {
-    height: string;
-    width:  string;
-    size:   string;
-    url:    string;
+export interface ImageVariant {
+  height: string;
+  width: string;
+  size: string;
+  url: string;
 }
 
-export interface DownsizedSmall {
-    height:   string;
-    width:    string;
-    mp4_size: string;
-    mp4:      string;
+export interface VideoVariant {
+  height: string;
+  width: string;
+  mp4_size: string;
+  mp4: string;
 }
 
-export interface FixedHeight {
-    height:    string;
-    width:     string;
-    size:      string;
-    url:       string;
-    mp4_size?: string;
-    mp4?:      string;
-    webp_size: string;
-    webp:      string;
-    frames?:   string;
-    hash?:     string;
-}
+export interface ImageFull {
+  height: string;
+  width: string;
+  size: string;
+  url: string;
 
-export enum Rating {
-    G = "g",
-    PG = "pg",
-}
+  mp4?: string;
+  mp4_size?: string;
 
-export enum Type {
-    GIF = "gif",
-}
+  webp: string;
+  webp_size: string;
 
+  frames?: string;
+  hash?: string;
+}
 export interface Meta {
-    status:      number;
-    msg:         string;
-    response_id: string;
+  status: number;
+  msg: string;
+  response_id: string;
 }
 
 export interface Pagination {
-    total_count: number;
-    count:       number;
-    offset:      number;
+  total_count: number;
+  count: number;
+  offset: number;
 }
